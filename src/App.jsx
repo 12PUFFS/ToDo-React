071e7122151d03c48631205109893202b08379f1
12PUffS <evgenypech98@gmail.com>
2025-05-29 13:47:40 +0300

    app style

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css';
 import TodoForm from './components/ToDo/TodoForm';
+import Button from './components/UI/Button';
 
 function App() {
   return (
     <>
       <div className="app">
         <TodoForm />
+        <Button />
       </div>
     </>
   );
---
+++ src/components/UI/Button.jsx
@@ -0,0 +1,10 @@
+import styles from '../UI/Button.module.css';
+function Button() {
+  return (
+    <div className={styles.button}>
+      <button>Submit</button>
+    </div>
+  );
+}
+
+export default Button;
