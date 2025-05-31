d35bbaf40bc7c37e6da76d79aeb8f8c5dd171024
12PUffS <evgenypech98@gmail.com>
2025-05-31 14:37:26 +0300

    todo-list adaptive 768

--- src/components/ToDo/TodoList.jsx
+++ src/components/ToDo/TodoList.jsx
@@ -5,10 +5,13 @@ import styles from './TodoList.module.css';
 
 function TodoList({ todos }) {
   return (
-    <div className={styles.todo}>
-      {todos.map((todo) => (
-        <Todo key={todo.id} todo={todo} />
-      ))}
+    <div className={styles.container}>
+      <div className={styles.todo}>
+        {todos.length === 0 && <h2>List is empty</h2>}
+        {todos.map((todo) => (
+          <Todo key={todo.id} todo={todo} />
+        ))}
+      </div>
     </div>
   );
 }
